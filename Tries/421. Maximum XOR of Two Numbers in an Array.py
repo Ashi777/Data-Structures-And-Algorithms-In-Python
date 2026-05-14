7e265d7517ebe8845a9c572107e977b923be0bb7
Ashi Malaiya <91681952+Ashi777@users.noreply.github.com>
2026-05-14 23:49:57 +0530

    Add files via upload

---
+++ Tries/421. Maximum XOR of Two Numbers in an Array.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self):
+        self.links=[None, None]
+
+    def containsKey(self, bit):
+        return self.links[bit] is not None
+
+    def get(self, bit):
+        return self.links[bit]
+
+    def put(self, bit, node):
+        self.links[bit]=node
+
+class Trie:
+    def __init__(self):
+        self.root=Node()
+
+    def insert(self, num):
+        node=self.root
+        for i in range(31, -1, -1):
+            bit=(num>>i)&1
+            if not node.containsKey(bit):
+                node.put(bit, Node())
+            node=node.get(bit)
+
+    def getMax(self, num):
+        node=self.root
+        maxNum=0
+        for i in range(31, -1, -1):
+            bit=(num>>i)&1
+            if node.containsKey(1-bit):
+                maxNum = maxNum | (1<<i)
+                node=node.get(1-bit)
+            else:
+                node=node.get(bit)
+        return maxNum
+
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        trie=Trie()
+        for num in nums:
+            trie.insert(num)
+        maxi=0
+        for num in nums:
+            maxi=max(maxi, trie.getMax(num))
+        return maxi
+
